9a0827a2fc450e1936f0961cb2ad0823ce4245ef
dubz <siencebro@gmail.com>
2024-03-20 00:37:42 +0300

    remove Map import

--- core/src/com/mygdx/game/Human.java
+++ core/src/com/mygdx/game/Human.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.Texture;
 // import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.maps.Map;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
