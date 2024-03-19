694ff0d9551fb1d7878e8b481092f2f52abceb68
dubz <siencebro@gmail.com>
2024-03-20 00:42:18 +0300

    add tileset and Tiled map to assets dir

---
+++ assets/tileset/Overworld Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="48" columns="6">
+ <image source="dosdos1.png" width="192" height="256"/>
+</tileset>
