beae6194f64001c32916920c7d612f046b53720e
dubz <siencebro@gmail.com>
2024-04-04 17:26:23 +0300

    fps to 60

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
   public static void main(String[] arg) {
     Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
     config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
-    config.setForegroundFPS(160);
+    config.setForegroundFPS(60);
     config.setTitle("TheSurvivor");
     new Lwjgl3Application(new Main(), config);
   }
